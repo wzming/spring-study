bc80343b62a3f62cf665dbc9a78643f7770d6a18
王知明 <wzmlove1121@outlook.com>
2021-12-16 15:42:47 +0800

    init

---
+++ src/test/java/com/aichunya/spring/study/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aichunya.spring.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
